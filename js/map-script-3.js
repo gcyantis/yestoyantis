545694cb8a1bcac8af0fddbd091fef1f5c46cb7c
gcyantis <colin@gcyantis.com>
2018-11-19 22:07:11 -0500

    Initial Commit

---
+++ js/map-script-3.js
@@ -0,0 +1,242 @@
+/*==============================
+	- MAP JS (ICON MARKER)
+	- Template: MARRY - Responsive HTML Wedding Template
+	- Author: DoubleEight
+	- Version: 1.0.0
+	- Website: themeforest.net/user/doubleeight/portfolio
+================================= */
+
+(function($) {
+
+'use strict';
+
+	// CHECK WINDOW RESIZE
+	var is_windowresize = false;
+	$(window).resize(function(){
+		is_windowresize = true;
+	});
+
+
+	//INITIALIZE MAP
+	function initialize() {
+
+		// Create an array of styles
+		//=======================================================================================
+  		var styles = [
+    		{
+      			stylers: [
+        			{ hue: "#f98d8a" },
+        			{ saturation: -50 }
+      			]
+    		}
+  		];
+
+		// Create a new StyledMapType object, passing it the array of styles,
+  		// as well as the name to be displayed on the map type control.
+  		var styledMap = new google.maps.StyledMapType(styles,
+   			{name: "Styled Map"});
+
+
+		//DEFINE MAP OPTIONS
+		//=======================================================================================
+  		var mapOptions = {
+    		zoom: 8,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+    		center: new google.maps.LatLng(48.089162,0.3236737),
+			panControl: true,
+  			zoomControl: true,
+  			mapTypeControl: true,
+  			//scaleControl: false,
+  			streetViewControl: true,
+  			overviewMapControl: true,
+			//rotateControl:true,
+			scrollwheel: false,
+
+  		};
+
+		//CREATE NEW MAP
+		//=======================================================================================
+  		var map = new google.maps.Map(document.getElementById('map-canvas-3'), mapOptions);
+
+		//Associate the styled map with the MapTypeId and set it to display.
+ 		 map.mapTypes.set('map_style', styledMap);
+ 		 map.setMapTypeId('map_style');
+
+		//MARKER ICON
+		//=======================================================================================
+		//var image = 'facebook30.svg';
+
+		//ADD NEW MARKER
+		//=======================================================================================
+  		/*var marker = new google.maps.Marker({
+    		position: map.getCenter(),
+   		 	map: map,
+    		title: 'Click to zoom',
+			icon: image
+  		});
+
+		var marker1 = new google.maps.Marker({
+    		position: new google.maps.LatLng(35.441938, -97.432494),
+   		 	map: map,
+    		title: 'Click to zoom'
+  		});*/
+
+
+		//ADD NEW MARKER WITH LABEL
+		//=======================================================================================
+		var marker1 = new MarkerWithLabel({
+       		position: new google.maps.LatLng(47.6350181,-1.0461798),
+       		draggable: false,
+       		raiseOnDrag: false,
+       		icon: ' ',
+       		map: map,
+         	labelContent: '<div id="wedding-marker" class="main-icon-wrapper"><div class="big-circle scale-animation"></div><div class="main-icon-text">The Wedding</div></div>',
+       		labelAnchor: new google.maps.Point(88, 88),
+       		labelClass: "labels" // the CSS class for the label
+     	});
+
+		var marker2 = new MarkerWithLabel({
+       		position: new google.maps.LatLng(47.460528,-0.5488288),
+       		draggable: false,
+       		raiseOnDrag: false,
+       		icon: ' ',
+       		map: map,
+         	labelContent: '<div id="gift-marker" class="de-icon circle medium-size" style="background-color:#96d296;"><i class="de-icon-bus"></i></div>',
+       		labelAnchor: new google.maps.Point(27, 27),
+       		labelClass: "labels" // the CSS class for the label
+     	});
+
+		var marker3 = new MarkerWithLabel({
+       		position: new google.maps.LatLng(49.044216,2.5421437),
+       		draggable: false,
+       		raiseOnDrag: false,
+       		icon: ' ',
+       		map: map,
+         	labelContent: '<div id="bus-marker" class="de-icon circle medium-size" style="background-color:#f9db85;"><i class="de-icon-airport"></i></div>',
+       		labelAnchor: new google.maps.Point(27, 27),
+       		labelClass: "labels" // the CSS class for the label
+     	});
+
+			var marker4 = new MarkerWithLabel({
+	       		position: new google.maps.LatLng(47.1574188,-1.6062316),
+	       		draggable: false,
+	       		raiseOnDrag: false,
+	       		icon: ' ',
+	       		map: map,
+	         	labelContent: '<div id="bus-marker" class="de-icon circle medium-size" style="background-color:#f9db85;"><i class="de-icon-airport"></i></div>',
+	       		labelAnchor: new google.maps.Point(27, 27),
+	       		labelClass: "labels" // the CSS class for the label
+	     	});
+
+
+			//INFO WINDOWS 2
+			//=======================================================================================
+			var contentString2 = ''+
+			'<div class="info-window-wrapper">'+
+				'<h6>Angers</h6>'+
+				'<div class="info-window-desc">This is where you can take the train to catch the bus to the chateau. We will also head in to town on Friday.</div>'+
+	      	'</div>';
+
+			var marker2_infowindow = new google.maps.InfoWindow({
+	      		content: contentString2,
+				maxWidth: 200,
+				pixelOffset: new google.maps.Size(0,-10)
+	  		});
+
+				//OPEN INFO WINDOWS ON LOAD
+					//marker2_infowindow.open(map,marker2);
+
+			//ON MARKER CLICK EVENTS
+			google.maps.event.addListener(marker2, 'click', function() {
+				marker2_infowindow.open(map,marker2);
+	  		});
+
+		//INFO WINDOWS 1
+		//=======================================================================================
+		var contentString1 = ''+
+		'<div class="info-window-wrapper">'+
+			'<h6>Château de Challain-la-Potherie</h6>'+
+			'<div class="info-window-desc">This is where we will be for the ceremony.</div>'+
+			'<div class="info-window-link"><a href="https://www.chateauchallain.com/" class="grey-link with-underline" target="_blank">Click Here</a></div>'+
+      	'</div>';
+
+		var marker1_infowindow = new google.maps.InfoWindow({
+      		content: contentString1,
+			maxWidth: 200,
+			pixelOffset: new google.maps.Size(0,-10)
+  		});
+
+		//OPEN INFO WINDOWS ON LOAD
+  		marker1_infowindow.open(map,marker1);
+
+		//ON MARKER CLICK EVENTS
+		google.maps.event.addListener(marker1, 'click', function() {
+			marker1_infowindow.open(map,marker1);
+  		});
+
+
+
+
+
+		//INFO WINDOWS 3
+		//=======================================================================================
+		var contentString3 = ''+
+		'<div class="info-window-wrapper">'+
+			'<h6>Charles de Gaulle Airport</h6>'+
+			'<div class="info-window-desc">Catch the TGV train from the airport!</div>'+
+			'<div class="info-window-link"><a href="https://www.raileurope.com/train/tgv-7537" class="grey-link with-underline" target="_blank">Train Schedule</a></div>'+
+      	'</div>';
+
+		var marker3_infowindow = new google.maps.InfoWindow({
+      		content: contentString3,
+			maxWidth: 200,
+			pixelOffset: new google.maps.Size(0,-10)
+  		});
+
+
+		//ON MARKER CLICK EVENTS
+		google.maps.event.addListener(marker3, 'click', function() {
+			marker3_infowindow.open(map,marker3);
+  		});
+
+			//INFO WINDOWS 4
+			//=======================================================================================
+			var contentString4 = ''+
+			'<div class="info-window-wrapper">'+
+				'<h6>Nantes Atlantique Airport</h6>'+
+				'<div class="info-window-desc">Take the shuttle into Nantes to catch the train to Angers.</div>'+
+				'<div class="info-window-link"><a href="https://www.raileurope.com/train-tickets/journeys/article/angers-nantes" class="grey-link with-underline" target="_blank">Train Schedule</a></div>'+
+					'</div>';
+
+			var marker4_infowindow = new google.maps.InfoWindow({
+						content: contentString4,
+				maxWidth: 200,
+				pixelOffset: new google.maps.Size(0,-10)
+				});
+
+
+			//ON MARKER CLICK EVENTS
+			google.maps.event.addListener(marker4, 'click', function() {
+				marker4_infowindow.open(map,marker4);
+				});
+
+
+		//ON BOUND EVENTS AND WINDOW RESIZE
+		//=======================================================================================
+		google.maps.event.addListener(map, 'bounds_changed', function() {
+			if (is_windowresize)
+			{
+				//map.setCenter(marker.getPosition());
+				window.setTimeout(function() {
+      				map.panTo(marker1.getPosition());
+    			}, 500);
+			}
+			is_windowresize=false;
+		});
+	}
+
+	// LOAD GMAP
+	google.maps.event.addDomListener(window, 'load', initialize);
+
+
+})(jQuery);
